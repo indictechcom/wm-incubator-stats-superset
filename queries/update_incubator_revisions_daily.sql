4bada34aad5f9079bc2bf5354ade63cee016c425
kcvelaga <kcvelaga@gmail.com>
2025-05-02 14:56:28 +0300

    update py and sh scripts

---
+++ queries/update_incubator_revisions_daily.sql
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ~/www/python/venv/bin/activate
+cd ~/www/python/src/scripts
+python3 update_incubator_revisions_table_daily.py
--- scripts/update_incubator_revisions_table_daily.py
+++ scripts/update_incubator_revisions_table_daily.py
@@ -1,22 +1,62 @@
-import json
-import os
+from datetime import datetime
+
 import urllib.request
-from datetime import date, datetime
 
 import pandas as pd
 import toolforge as forge
+
+from utils import (
+    clear_destination_table,
+    update_destination_table
+)
 
 user_agent = forge.set_user_agent(
     tool="Wikimedia Incubator Superset Dashboard",
     email="kcvelaga@gmail.com",
 )
+destination_table = "incubator_revisions_daily"
 
-with open("stats/logs.json", "r") as file:
-    logs = json.load(file)
+def get_query(url):
+    with urllib.request.urlopen(url) as response:
+        query = response.read().decode()
+    return query
 
+def fetch_data(query, db="incubatorwiki"):
+    
+    try:
+        conn = forge.connect(db)
+        with conn.cursor() as cur:
+            cur.execute(query)
+            result = cur.fetchall()
 
-sql_query_url = (
-    "https://raw.githubusercontent.com/indictechcom/wm-incubator-stats/main/query.sql"
-)
-with urllib.request.urlopen(sql_query_url) as response:
-    query = response.read().decode()
+        df = pd.DataFrame(result)
+        df["prefix"] = df["prefix"].apply(lambda x: x.decode("utf-8"))
+    except:
+        df = None
+    
+    return df
+
+def main():
+
+    query = get_query("https://raw.githubusercontent.com/indictechcom/wm-incubator-stats-superset/refs/heads/main/queries/query.sql")
+    df = fetch_data(query)
+
+    if df != None:
+
+        con = forge.toolsdb("s56696__incubator_stats_superset_p")
+        cur = con.cursor()
+        try:
+            clear_destination_table(destination_table, cur)
+            update_destination_table(df, destination_table, cur)
+            con.commit()
+            print(f"incubator revisions daily table updateda at: {datetime.now()}.")
+        except Exception as e:
+            con.rollback()
+            print(f"update failed due to: {e}")
+        finally:
+            cur.close()
+            con.close()
+
+if __name__ == "__main__":
+    main()
+    
