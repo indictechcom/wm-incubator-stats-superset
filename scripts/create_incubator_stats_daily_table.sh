9130e0a5eafb7dca23d91f18b86391dbf3207661
kcvelaga <kcvelaga@gmail.com>
2025-05-03 18:33:33 +0300

    update db

--- scripts/create_incubator_stats_daily_table.sh
+++ scripts/create_incubator_stats_daily_table.sh
@@ -4,7 +4,7 @@ CNF_PATH="$HOME/replica.my.cnf"
 
 SQL_FILE="$HOME/www/python/src/queries/create_incubator_stats_daily.sql"
 
-DATABASE="s56696__incubator_stats_superset_p"
+DATABASE="s56696__incubator_stats_daily_p"
 HOST="tools.db.svc.wikimedia.cloud"
 
 mariadb --defaults-file="$CNF_PATH" -h "$HOST" "$DATABASE" < "$SQL_FILE" && \
