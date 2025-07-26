0e36dd7fb951546ab8851a163bba1b2f52239b78
satvshr <satvmishi@gmail.com>
2025-07-26 16:28:49 +0530

    Made requested changes

--- queries/generate_incubator_stats_monthly_lang.sql
+++ queries/generate_incubator_stats_monthly_lang.sql
@@ -2,20 +2,13 @@ WITH base AS (
     SELECT
         rev.rev_id,
         rev.rev_timestamp,
-        SUBSTRING_INDEX(
-          REGEXP_SUBSTR(page.page_title, 'W[a-z]/[a-z]+'),
-          'W[a-z]/', 1
-        ) AS project_code,
-        SUBSTRING_INDEX(
-          REGEXP_SUBSTR(page.page_title, 'W[a-z]/[a-z]+'),
-          '/', -1
-        ) AS language_code,
+        REGEXP_SUBSTR(page.page_title, 'W[a-z]') AS project_code,
+        SUBSTRING_INDEX(REGEXP_SUBSTR(page.page_title, 'W[a-z]/[a-z]+'), '/', -1) AS language_code,
         actor.actor_id
     FROM revision AS rev
     INNER JOIN page   ON rev.rev_page   = page.page_id
     INNER JOIN actor  ON rev.rev_actor  = actor.actor_id
     INNER JOIN user   ON actor.actor_name = user.user_name
-    LEFT  JOIN recentchanges AS rc ON rev.rev_id     = rc.rc_this_oldid
     WHERE page.page_namespace   IN (0,1,10,11,14,15,828,829)
       AND page.page_is_redirect  = 0
       AND user.user_name NOT IN {EXCL_USERS}
@@ -30,21 +23,23 @@ WITH base AS (
 monthly_edits AS (
     SELECT
         DATE_FORMAT(rev_timestamp, '%Y-%m-01') AS month_start,
+        project_code,
         language_code,
         actor_id,
         COUNT(DISTINCT rev_id) AS edits_in_month
     FROM base
-    GROUP BY month_start, language_code, actor_id
+    GROUP BY month_start, project_code, language_code, actor_id
 ),
 
 monthly_active AS (
     SELECT
         month_start,
+        project_code,
         language_code,
         SUM(CASE WHEN edits_in_month >= 5  THEN 1 ELSE 0 END) AS monthly_active_editors_min5,
         SUM(CASE WHEN edits_in_month >= 15 THEN 1 ELSE 0 END) AS monthly_active_editors_min15
     FROM monthly_edits
-    GROUP BY month_start, language_code
+    GROUP BY month_start, project_code, language_code
 )
 
 SELECT
